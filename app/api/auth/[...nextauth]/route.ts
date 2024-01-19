d597d2d3eaccf1d246c62fb12062b3e1d9380861
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-01-19 12:11:50 +0530

    authOptions error removing trial 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { Account, User as AuthUser } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import User from "@/models/user";
-import { connectMongoDB } from "@/lib/mongodb";
+import NextAuth from "next-auth";
+import authOptions from "@/lib/configs/auth/authOptions";
 
-
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: any) {
-        await connectMongoDB();
-        try {
-          const user = await User.findOne({ email: credentials.email });
-          if (user) {
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password,
-              user.password
-            );
-            if (isPasswordCorrect) {
-              return user;
-            }
-          }
-        } catch (err: any) {
-          throw new Error(err);
-        }
-      },
-    }),
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID ?? "",
-    //   clientSecret: process.env.GITHUB_SECRET ?? "",
-    // }),
-    // ...add more providers here
-  ],
-  callbacks: {
-    // async signIn({ user, account }: { user: AuthUser; account: Account }) {
-    //   if (account?.provider == "credentials") {
-    //     return true;
-    //   }
-
-    // },
-    async signIn({user,account}) {
-      if(account?.provider == "credentials"){
-        return true;
-      }else{
-        return false;
-      }
-    },
-  },
-};
 
 export const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ lib/configs/auth/authOptions.ts
@@ -0,0 +1,58 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcryptjs'
+
+
+const authOptions: NextAuthOptions = {
+    // Configure one or more authentication providers
+    providers: [
+      CredentialsProvider({
+        id: "credentials",
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "text" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials: any) {
+          await connectMongoDB();
+          try {
+            const user = await User.findOne({ email: credentials.email });
+            if (user) {
+              const isPasswordCorrect = await bcrypt.compare(
+                credentials.password,
+                user.password
+              );
+              if (isPasswordCorrect) {
+                return user;
+              }
+            }
+          } catch (err: any) {
+            throw new Error(err);
+          }
+        },
+      }),
+      // GithubProvider({
+      //   clientId: process.env.GITHUB_ID ?? "",
+      //   clientSecret: process.env.GITHUB_SECRET ?? "",
+      // }),
+      // ...add more providers here
+    ],
+    callbacks: {
+      // async signIn({ user, account }: { user: AuthUser; account: Account }) {
+      //   if (account?.provider == "credentials") {
+      //     return true;
+      //   }
+  
+      // },
+      async signIn({user,account}) {
+        if(account?.provider == "credentials"){
+          return true;
+        }else{
+          return false;
+        }
+      },
+    },
+  };
+export default authOptions;
